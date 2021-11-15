fa774a37ce02aa5db2cbc212a78718eabbbd2854
Jeffrey Ma <jeffrey.ma@gllue.com>
2021-11-15 21:27:51 +0800

    :bookmark: Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/DoliteMatheo/JAIKit.git",
-    version="0.0.3",
+    version="0.0.4",
     zip_safe=False,
     extras_require={"plot": ["tensorflow", "matplotlib"]}
 )
